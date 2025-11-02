4bb48b44af7ae409df4c3ed983a31c884b02cb58
Saluk Mudalige <salukmudhalige321@gmail.com>
2025-11-02 19:11:17 +0530

    Added verification after the login

--- cypress/e2e/OrangeHRM-Login/login.cy.js
+++ cypress/e2e/OrangeHRM-Login/login.cy.js
@@ -2,14 +2,17 @@
 import Login from "../../PageObjects/LoginPage.js";
 
 describe('OrangeHRM Login', ()=> {
-    it('', ()=> {
+    it('verify user should be able to login Orange HRM sucessfully', ()=> {
         cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
         cy.fixture('orange-hrm').then((data)=>{
             const ln = new Login();
             ln.SetUsername(data.username)
             ln.SetPassword(data.password)
             ln.ClickSubmitBtn()
+            cy.get('h6', { timeout: 6000 }).should('contain', "Dashboard");
         })
 
     })
+
+
 })
