7b147bebd839e7574f8eb838625766d3ff61cef5
Saluk Mudalige <salukmudhalige321@gmail.com>
2025-11-05 21:05:49 +0530

    added invalid credentials login validation

--- cypress/e2e/OrangeHRM-Login/login.cy.js
+++ cypress/e2e/OrangeHRM-Login/login.cy.js
@@ -2,8 +2,11 @@
 import Login from "../../PageObjects/LoginPage.js";
 
 describe('OrangeHRM Login', ()=> {
-    it('verify user should be able to login Orange HRM sucessfully', ()=> {
+    before(()=>{
         cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+    })
+
+    it('verify user should be able to login Orange HRM sucessfully', ()=> {
         cy.fixture('orange-hrm').then((data)=>{
             const ln = new Login();
             ln.SetUsername(data.username)
@@ -11,7 +14,17 @@ describe('OrangeHRM Login', ()=> {
             ln.ClickSubmitBtn()
             cy.get('h6', { timeout: 6000 }).should('contain', "Dashboard");
         })
+    })
 
+        it('verify error message should display when user enter incorrect credentials', ()=> {
+        cy.fixture('data/Users/InvalidUser.json').then((data)=>{
+            cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+            const ln = new Login();
+            ln.SetUsername(data.username)
+            ln.SetPassword(data.password)
+            ln.ClickSubmitBtn()
+            cy.get('.oxd-alert-content > .oxd-text').should('contain', data.message)
+        })
     })
 
 
